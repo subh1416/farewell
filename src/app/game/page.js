798de01832c19f0e001a1a4ded5e20e74c382980
subh1416 <subhadipsamanta1416@gmail.com>
2024-05-02 00:45:54 +0530

    done

--- src/app/game/page.js
+++ src/app/game/page.js
@@ -1,15 +1,9 @@
 "use client";
-
 import * as React from 'react';
 import { useState } from "react";
 import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
-import CircularProgress from '@mui/material/CircularProgress';
-import Skeleton from '@mui/material/Skeleton';
-
-
-
 
 export default function Game() {
   const [randomNumber, setRandomNumber] = useState(null);
@@ -18,7 +12,25 @@ export default function Game() {
   const [randomNumber2, setRandomNumber2] = useState(null);
   const [usedNumbers2, setUsedNumbers2] = useState(new Set());
 
+  const [clickCount, setClickCount] = useState(0);
+
   const generateRandomNumber = () => {
+    setClickCount(prevCount => prevCount + 1);
+    if (clickCount === 0) {
+      setRandomNumber(54);
+      setRandomNumber2(2);
+      return;
+    }
+
+    // add here for fixing number
+    // eg:-
+    // if (clickCount === 1) {
+    //   setRandomNumber(34);
+    //   setRandomNumber2(3);
+    //   return;
+    // }
+
+    
     if (usedNumbers.size === 68) {
       alert("No Senior left!");
       return;
@@ -36,13 +48,9 @@ export default function Game() {
     let newRandomNumber2;
     do {
       newRandomNumber2 = Math.floor(Math.random() * 68) + 1;
-    } while (usedNumbers2.has(newRandomNumber2) || newRandomNumber === newRandomNumber2); // Ensure the second number is different
-
+    } while (usedNumbers2.has(newRandomNumber2) || newRandomNumber === newRandomNumber2); 
     setRandomNumber2(newRandomNumber2);
     setUsedNumbers2((prevUsedNumbers) => new Set([...prevUsedNumbers, newRandomNumber2]));
-  
-
-    
   };
 
   return (
@@ -81,10 +89,6 @@ export default function Game() {
           >
             Start
           </Button>
-
-          
-
-         
 
         </Box>
       </Box>
